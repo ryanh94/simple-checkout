7f8c8e4c3e0c58f282a25d913ea16ecb8f8e199c
Ryan Hughes <RyanBH1994@hotmail.co.uk>
2023-01-20 17:34:22 +0000

    passing tests

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+from collections import Counter
+from typing import List
+from products import ProductSource
 
-def checkout():
-    pass
+
+def checkout(product_source: ProductSource, basket: List[str]) -> int:
+    product_list = product_source.get_product_list()
+    
+    price = workout_price(product_list, basket)
+    return price
+
+def workout_price(product_list, basket):
+    basket_occurances = Counter(basket)
+    price = 0
+    for product in product_list:
+        if product["Item"] in basket_occurances:
+            quantity = basket_occurances[product["Item"]]
+            
+            special_offer = product["Special Price"].split(" ")
+            quantity_needed_for_offer = int(special_offer[0])
+            offer_price = int(special_offer[2])
+            if quantity >= quantity_needed_for_offer:
+                number_of_offers = int(quantity/quantity_needed_for_offer)
+                price += (offer_price * number_of_offers)
+                remainder_items = quantity % quantity_needed_for_offer
+                price += remainder_items * int(product["Unit Price"])
+            else:
+                price += quantity * int(product["Unit Price"])
+    
+    return price
+
--- tests/unit/test_checkout.py
+++ tests/unit/test_checkout.py
@@ -1,6 +1,13 @@
+from main import checkout
+from products import ProductsFromCsv
+
 
 def test_checkout_correct_price():
-    assert 1 == 2
+    product_source = ProductsFromCsv("tests\\unit\\product_list.csv")
+    checkout_price = checkout(product_source, ["A", "B", "B"])
+    assert checkout_price == 95
 
-def test_checkout_incorrect_price():
-    assert 1 == 2
+def test_checkout_multiple_offers_correct_price():
+    product_source = ProductsFromCsv("tests\\unit\\product_list.csv")
+    checkout_price = checkout(product_source, ["A", "B", "B", "B", "B", "B"])
+    assert checkout_price == 170
